3dcc98f5e7c8df9d2be39e178fb78e8965089644
Tim Marchant <fromtimwithcode@gmail.com>
2021-08-04 18:33:37 -0500

    Completed Part 3 1-3

--- components/Menu.js
+++ components/Menu.js
@@ -31,3 +31,19 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+
+const header = document.querySelector('.header');
+const menuButton = document.querySelector('.menu-button');
+
+// create menu
+function menuMaker(array) {
+
+  const menu = document.createElement('div');
+  const list = document.createElement('ul');
+
+  array.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.textContent = item;
+    list.appendChild(listItem);
+  });
+}
